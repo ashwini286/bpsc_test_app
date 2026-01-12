f26eddfe475077e1481deb84819fae37c799f540
Ashwini kumari <ashwini.swe@gmail.com>
2026-01-12 21:25:54 +0530

    chore: remove unnecessary files and update browser tab icon

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders BPSC test portal', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Online MCQ Test Portal for BPSC & STET/i);
+  expect(headingElement).toBeInTheDocument();
 });
